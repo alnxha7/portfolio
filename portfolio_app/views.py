43bc285e224a2037a20a72a0e286b80ec3b9b54b
alnxha7 <129566733+alnxha7@users.noreply.github.com>
2025-04-09 14:49:55 +0530

    Update views.py

--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -33,7 +33,7 @@ def home(request):
 def contact(request):
     if request.method == 'POST':
         configuration = sib_api_v3_sdk.Configuration()
-        configuration.api_key['api-key'] = "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"
+        configuration.api_key['api-key'] = "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"
         api_instance = sib_api_v3_sdk.TransactionalEmailsApi(sib_api_v3_sdk.ApiClient(configuration))
 
         subject = "New Message from Portfolio Visit"
